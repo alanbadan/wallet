2a8dcf4b9fd4f87c050364ef26e363ac5002229e
alanbadan <65678052+alanbadan@users.noreply.github.com>
2022-03-07 18:29:53 -0300

    first commit

---
+++ src/test/java/com/wallet/walletapi/WalletapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wallet.walletapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WalletapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
